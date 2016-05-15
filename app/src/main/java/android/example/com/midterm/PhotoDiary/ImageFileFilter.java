f262f07312fe8deaee37e8203383b0ef329adeed
donghyuk92 <donghyuk92@naver.com>
2016-05-15 11:57:00 +0900

    first

---
+++ app/src/main/java/android/example/com/midterm/PhotoDiary/ImageFileFilter.java
@@ -0,0 +1,24 @@
+package android.example.com.midterm.PhotoDiary;
+
+import java.io.File;
+import java.io.FileFilter;
+
+public class ImageFileFilter implements FileFilter {
+	private final String[] okFileExtensions = new String[] { "jpg", "png", "gif", "jpeg" };
+
+	/**
+     * 
+     */
+	public ImageFileFilter() {
+	}
+
+	public boolean accept(File file) {
+		for (String extension : okFileExtensions) {
+			if (file.getName().toLowerCase().endsWith(extension) && file.length() != 0) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
---
+++ app/src/main/java/android/example/com/midterm/PhotoDiary/ImageGridActivity.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.example.com.midterm.PhotoDiary;
+
+import android.example.com.midterm.PhotoDiary.util.Utils;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.appcompat.BuildConfig;
+import android.util.Log;
+
+/**
+ * Simple FragmentActivity to hold the main {@link ImageGridFragment} and not much else.
+ */
+public class ImageGridActivity extends FragmentActivity {
+    private static final String TAG = "ImageGridActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        if (BuildConfig.DEBUG) {
+            Utils.enableStrictMode();
+        }
+        super.onCreate(savedInstanceState);
+
+        if (getSupportFragmentManager().findFragmentByTag(TAG) == null) {
+        	final FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(android.R.id.content, new ImageGridFragment(), TAG);
+            ft.commit();
+        }
+    }
+    
+    @Override
+    protected void onResume() {
+    	// TODO Auto-generated method stub
+    	super.onResume();
+    	Log.e("Fragment", "Onresume");
+    	ImageGridFragment resultFrag = (ImageGridFragment) getSupportFragmentManager().findFragmentByTag(TAG);
+		if (resultFrag != null) {
+			Log.e("Fragment", "Found");
+		    resultFrag.refresh();
+		}
+    }
+}
